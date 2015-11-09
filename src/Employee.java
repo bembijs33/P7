c3c6e522fcebf38aae34ff7af9bf90929eb5e252
bembijs33 <rakvicsr@gmail.com>
2015-11-09 10:19:40 +0100

    s

---
+++ src/Employee.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+	public String firstName; 
+	public String lastName;
+	public String phoneNumber;
+	public String role;
+	
+public Employee(String firstName, String lastName, String string, String role){
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.phoneNumber=string;
+		this.role=role;
+}
+
+
+
+public void setName(){
+	
+	}
+
+public void changePhoneNumber(){
+	
+	}
+
+public void changeRole(){
+	
+	}
+public toString()
+public static void main (String[]args){
+	List<Employee> employee = new ArrayList<Employee>();
+
+	Employee numb1 = new Employee("Rob", "oeb", "555", "ww" );
+	
+	
+	employee.add(numb1);
+	
+	
+	System.out.println(employee);
+	
+	}
+}
--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,9 @@
 
 public class Test {
-	public static void main (String[]args){
-		System.out.println("hello world");
-		System.out.println("hello to you too");
-		System.out.println("conflict");
+	//public static void main (String[]args){
+		//System.out.println("hello world");
+		//System.out.println("hello to you too");
+		//System.out.println("conflict");
 // leave the comment, to check if you are committing :)
-	}
+	//}
 }
