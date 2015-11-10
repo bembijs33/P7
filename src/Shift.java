d4e1264e97d6f73ea6c5e7ca2b8e3958a51a5c9d
JHP91 <poulsen_business@live.dk>
2015-11-10 15:26:26 +0100

    dawda

---
+++ src/Shift.java
@@ -0,0 +1,7 @@
+
+public class Shift {
+
+	
+	int shiftID;
+	
+}
--- src/Skill.java
+++ src/Skill.java
@@ -9,6 +9,11 @@ public class Skill {
 	public Skill(String addSkill){
 			Skill.addSkill=addSkill;
 	
+	public String toString(){
+				String s = "addSkill: " + addSkill;
+				return s;
+			}
+	//How do I get this toString thing to work?! I DONT KNOW WHAT IM DOING.
 		
 }
 }
