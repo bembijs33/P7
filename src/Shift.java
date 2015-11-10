31eab2a61e40d9c0616118c6910781b6693fac34
JHP91 <poulsen_business@live.dk>
2015-11-10 15:35:01 +0100

    created new classes and changes

--- src/Shift.java
+++ src/Shift.java
@@ -3,5 +3,13 @@ public class Shift {
 
 	
 	int shiftID;
+	// time: date - I do not know how to do this? 
 	
+	public void getCleaningScheduleID () {
+		
+		
+	}
+	public void addEmployee () {
+		
+	}
 }
