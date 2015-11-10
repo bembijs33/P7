a324db190af7d756e14b3e74617d69c8f4af875b
JHP91 <poulsen_business@live.dk>
2015-11-10 14:41:47 +0100

    added skill class. Don't know if its right? But I tried :/

---
+++ src/Skill.java
@@ -0,0 +1,18 @@
+
+public class Skill {
+
+	int skillID;
+	//list of skill ID's 
+
+	public static String firstName; 
+	public static String lastName;
+	
+	public static void Employee(String firstName, String lastName){
+			
+	Employee.firstName=firstName;
+	Employee.lastName=lastName;
+	
+	}
+}
+
+
