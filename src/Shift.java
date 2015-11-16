c011f8e6fec9265c1e791559006b480213a4217f
JHP91 <poulsen_business@live.dk>
2015-11-16 09:20:25 +0100

    changes to shift.

--- src/Shift.java
+++ src/Shift.java
@@ -9,12 +9,10 @@ public class Shift {
 
 	
 	
-	public void addDateTime (int Date, int Time) {
-		this.Date=Date;
-		this.Time=Time;
-		//Heeeelp Roberts!!
-		// What you do is that when calling method addDateTime you need to inert two variables int, that are called Date and Time
-		// this.Date=Date is not working, because Date time(which is declared) is not the same as int Date (what you are assking for)
+	public Date changeDateTime (Date time) {
+		this.time=time;
+	
+		return time;
 	}
 	 
 	
