c9b4fcd298ae44704540e04b21341fc2e04aed14
Heidi <heidin89@hotmail.com>
2015-11-27 10:12:41 +0100

    update from wednesday

--- src/Shift.java
+++ src/Shift.java
@@ -3,12 +3,22 @@ import java.util.Date;
 public class Shift {
 
 	int shiftID;
-	Date time;
+	Date Start;
+	Date End;
+	CleaningSchedule Schedule;
+	
+	
+	public Shift(int id, Date start, Date end, CleaningSchedule schedule) {
+		this.shiftID = id;
+		this.Start = start;
+		this.End = end;
+		this.Schedule = schedule;
+	}
 
 
 
 	public Date changeDateTime (Date time) {
-		this.time=time;
+		//this.time=time;
 	
 		return time;
 
