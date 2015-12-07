6dfb56e8608d597e251a75e35e7ef5cf87d7c23c
bembijs33 <rakvicsr@gmail.com>
2015-12-07 16:58:26 +0100

    no message

--- src/Employee.java
+++ src/Employee.java
@@ -1,26 +1,21 @@
 import java.util.ArrayList;
 
-import javax.swing.DefaultListModel;
-
 public class Employee {
 	public  String firstName; 
 	public String lastName;
 	public int phoneNumber;
-	//public DefaultListModel<Skill> skillList = new DefaultListModel<Skill>();
 	public ArrayList<Skill> skillList = new ArrayList<Skill>(); // Instantiating arrayList for skills
-	//And perhaps working hours allowed?
+
 
 	public Employee(String firstName, String lastName, int phoneNumber){
 		this.firstName=firstName;
 		this.lastName=lastName;
 		this.phoneNumber=phoneNumber;
-		//Should skill be somewhere here?  
-		//Rob: Employee is created without any skill. It is added afterwards in method AddSkill(). 
 		}
 
 	public String toString(){
 		return " " + firstName
-			+ " " + lastName; 
+			+ "      " + lastName + "       Phone:   " + phoneNumber; 
 			
 		}
 	
